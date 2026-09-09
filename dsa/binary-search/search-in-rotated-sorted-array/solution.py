60ba9b4fa4b3380ea985beaa1d27076dd1779d93
Philip William Ventura <philipwilliamventura@gmail.com>
2026-09-09 13:19:23 -0400

    Implement search-in-rotated-sorted-array

---
+++ dsa/binary-search/search-in-rotated-sorted-array/solution.py
@@ -0,0 +1,23 @@
+# Pattern: Binary Search
+# Time: O(log(n)) | Space: O(1)
+# Tripped up on: Divide in two cases - are we in the left sorted portion or the right sorted portion?
+#                in the two cases, divide in another two cases - do we go left or right?
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums)-1
+        while l <= r:
+            mid = l + (r-l) // 2
+            if target == nums[mid]:
+                return mid
+            
+            if nums[l] <= nums[mid]:
+                if target > nums[mid] or target < nums[l]:
+                    l = mid + 1
+                else:
+                    r = mid - 1
+            else:
+                if target < nums[mid] or target > nums[r]:
+                    r = mid - 1
+                else:
+                    l = mid + 1
+        return -1
