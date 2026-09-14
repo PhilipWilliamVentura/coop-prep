46b408f4217f71a1f23953abf014b3d0e2d6f222
Philip William Ventura <philipwilliamventura@gmail.com>
2026-09-14 17:21:11 -0400

    Reverse linked list

---
+++ dsa/linked-list/reverse-linked-list/solution.py
@@ -0,0 +1,20 @@
+# Pattern: Linked List iteration
+# Time: O(n) | Space: O(1)
+# Tripped up on: prev starts at None, curr starts at head, use a temp variable to store curr.next
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        prev, curr = None, head
+
+        while curr:
+            temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp
+        return prev
