04ef1e42c87b4fa45c1f02b1813c3ecaeea20df1
Philip William Ventura <philipwilliamventura@gmail.com>
2026-09-23 13:51:30 -0400

    Implement DFS binary tree problems

--- dsa/trees/same-binary-tree/solution.py
+++ dsa/trees/same-binary-tree/solution.py
@@ -10,7 +10,9 @@
 #         self.right = right
 
 class Solution:
-    def maxDepth(self, root: Optional[TreeNode]) -> int:
-        if root == None:
-            return 0
-        return 1+ max(self.maxDepth(root.left), self.maxDepth(root.right))
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if not p and not q:
+            return True
+        if not p or not q or p.val != q.val:
+            return False
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
