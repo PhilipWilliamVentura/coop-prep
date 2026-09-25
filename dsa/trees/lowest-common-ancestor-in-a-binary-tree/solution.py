62c781c2390f865a1fb972ccce1d822d92d011cc
Philip William Ventura <philipwilliamventura@gmail.com>
2026-09-25 18:59:44 -0400

    Implement lowest common ancestor in a binary tree

---
+++ dsa/trees/lowest-common-ancestor-in-a-binary-tree/solution.py
@@ -0,0 +1,20 @@
+# Pattern: Iteration
+# Time: O(log n) | Space: O(1) height of the tree
+# Tripped up on: if p and q are smaller than root go left, if p and q are bigger go right, else return root
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def lowestCommonAncestor(self, root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
+        while root:
+            if p.val > root.val and q.val > root.val:
+                root = root.right
+            elif p.val < root.val and q.val < root.val:
+                root = root.left
+            else:
+                return root
